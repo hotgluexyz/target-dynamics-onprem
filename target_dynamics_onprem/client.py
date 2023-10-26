a06030084c54d0e83db4fd5cf651d669ca39417e
Key <keynarafaelp@gmail.com>
2023-10-26 15:47:57 -0500

    log base_url

--- target_dynamics_onprem/client.py
+++ target_dynamics_onprem/client.py
@@ -31,6 +31,7 @@ class DynamicOnpremSink(HotglueSink):
         tenant = self.config.get('tenant')
         company_id = self.config.get('company_id')
         base_url = f"http://{url_base}/{tenant}/ODataV4/Company('{company_id}')"
+        self.logger.info(f"BASE URL: {base_url}")
         return base_url
 
     @property
