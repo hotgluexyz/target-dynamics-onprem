3aa3a00119fe928855e49883e1ceedab2f8b92ed
Key <keynarafaelp@gmail.com>
2024-01-04 15:26:10 -0500

    fix params

--- target_dynamics_onprem/client.py
+++ target_dynamics_onprem/client.py
@@ -57,7 +57,7 @@ class DynamicOnpremSink(HotglueSink):
     
     def request_api(self, http_method, endpoint=None, params={}, request_data=None, headers={}):
         """Request records from REST endpoint(s), returning response records."""
-        resp = self._request(http_method, endpoint, params, headers, request_data=request_data)
+        resp = self._request(http_method, endpoint, params=params, headers=headers, request_data=request_data)
         return resp
     
     def get_endpoint(self, record):
@@ -72,13 +72,12 @@ class DynamicOnpremSink(HotglueSink):
         factor=2,
     )
     def _request(
-        self, http_method, endpoint, auth, params, request_data=None, headers={}
+        self, http_method, endpoint, auth=None, params={}, request_data=None, headers={}
     ) -> requests.PreparedRequest:
         """Prepare a request object."""
         url = self.url(endpoint)
         headers.update(self.default_headers)
         headers.update({"Content-Type": "application/json"})
-        params = self.params
         data = (
             json.dumps(request_data, cls=HGJSONEncoder)
             if request_data
