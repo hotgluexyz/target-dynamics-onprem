e5f7dccc7f92cd3d04db34a6e8e29acc939f7d1d
Key <keynarafaelp@gmail.com>
2024-01-11 16:24:50 -0500

    fix sdl endpoint

--- target_dynamics_onprem/sinks.py
+++ target_dynamics_onprem/sinks.py
@@ -427,10 +427,9 @@ class PurchaseInvoices(DynamicOnpremSink):
                             )
                             pol_id = purchase_order_lines.json().get("id")
                             #set dimension lines
-                            self.logger.info(f"ENDPOINT FOR SDL {sdl_endpoint}")
                             for sdl in dimension_set_lines:
                                 sdl_endpoint = f"{pol_endpoint}({pol_id})/dimensionSetLines(df226f93-3ead-ed11-be65-6045bde95266)"
-                                self.logger.info(f"SDL ENDPOINTTT {sdl_endpoint}")
+                                self.logger.info(f"ENDPOINT FOR SDL {sdl_endpoint}")
                                 purchase_order_lines = self.request_api(
                                     "POST",
                                     endpoint=sdl_endpoint,
