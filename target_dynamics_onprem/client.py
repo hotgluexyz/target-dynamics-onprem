9775dcc95ad1ba75e501f7e35f33d2cfa2c2d003
Keyna Rafael <95432445+keyn4@users.noreply.github.com>
2024-04-25 12:52:54 -0500

    elevate right error if deletion of invoices fails, send attachment content to att patch (#9)

--- target_dynamics_onprem/client.py
+++ target_dynamics_onprem/client.py
@@ -66,9 +66,9 @@ class DynamicOnpremSink(HotglueSink):
         converted_date = date.split("T")[0]
         return converted_date
     
-    def request_api(self, http_method, endpoint=None, params={}, request_data=None, headers={}):
+    def request_api(self, http_method, endpoint=None, params={}, request_data=None, headers={}, json=True):
         """Request records from REST endpoint(s), returning response records."""
-        resp = self._request(http_method, endpoint, params=params, headers=headers, request_data=request_data)
+        resp = self._request(http_method, endpoint, params=params, headers=headers, request_data=request_data, json=json)
         return resp
     
     def get_endpoint(self, record, endpoint=None):
@@ -90,7 +90,7 @@ class DynamicOnpremSink(HotglueSink):
         factor=2,
     )
     def _request(
-        self, http_method, endpoint, auth=None, params={}, request_data=None, headers={}
+        self, http_method, endpoint, auth=None, params={}, request_data=None, headers={}, json=True
     ) -> requests.PreparedRequest:
         """Prepare a request object."""
         url = self.url(endpoint)
@@ -104,13 +104,20 @@ class DynamicOnpremSink(HotglueSink):
 
         self.logger.info(f"MAKING {http_method} REQUEST")
         self.logger.info(f"URL {url} params {params} data {request_data}")
+
+        kwargs = dict()
+        if json:
+            kwargs["json"] = request_data
+        else:
+            kwargs["data"] = request_data
+
         response = requests.request(
             method=http_method,
             url=url,
             params=params,
             headers=headers,
-            json=request_data,
-            auth=auth
+            auth=auth,
+            **kwargs
         )
         self.logger.info("response!!")
         self.logger.info(f"RESPONSE TEXT {response.text} STATUS CODE {response.status_code}")
@@ -158,13 +165,13 @@ class DynamicOnpremSink(HotglueSink):
             if not content:
                 if url:
                     response = requests.get(url)
-                    data = base64.b64encode(response.content)
+                    data = base64.b64encode(response.content).decode()
                 else:
                     att_path = f"{self.config.get('input_path')}/{attachment.get('id')}_{att_name}"
                     with open(att_path, "rb") as attach_file:
-                        data = base64.b64encode(attach_file.read())
+                        data = base64.b64encode(attach_file.read()).decode()
             else:
-                data = attachment.get("content").encode('utf-8')
+                data = attachment.get("content")
             
             content_payload = {"attachmentContent": data}
 
@@ -179,8 +186,9 @@ class DynamicOnpremSink(HotglueSink):
                 att = self.request_api(
                     "PATCH",
                     endpoint=f"{endpoint}({att_id})/attachmentContent",
-                    request_data=content_payload,
-                    headers={"If-Match": "*"}
+                    request_data=data,
+                    headers={"If-Match": "*"},
+                    json=False
                 )
                 self.logger.info(f"Attachment for parent {parent_id} posted succesfully with id {att_id}")
 
