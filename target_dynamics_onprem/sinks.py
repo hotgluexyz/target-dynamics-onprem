f34d9721a45bd0f23e9cb83d47b847939d8b95f4
Key <keynarafaelp@gmail.com>
2024-07-29 15:04:38 -0400

    get all att

--- target_dynamics_onprem/sinks.py
+++ target_dynamics_onprem/sinks.py
@@ -326,7 +326,7 @@ class PurchaseInvoices(DynamicOnpremSink):
 
     def preprocess_record(self, record: dict, context: dict) -> None:
         self.logger.info(f"Fetching attachment data...")
-        url = "/attachments(a9d8bbea-a024-ef11-be6b-6045bde95266)/attachmentContent"
+        url = "/attachments?$filter=parentId eq 3bf2f72a-cd4d-ef11-98c4-6045bdaa646f and parentType eq 'Purchase_x0020_Invoice'"
         response = self.request_api("GET", self.get_endpoint(record, endpoint=url))
         self.logger.info(f"Attachment response!! -> {response.text}")
         raise Exception(f"Finisinh test job")
